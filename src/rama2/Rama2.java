a4a09442160d3e54853e0b115e4279edef3992d3
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2020-01-15 14:02:42 +0100

    segundo commit

--- src/rama2/Rama2.java
+++ src/rama2/Rama2.java
@@ -16,6 +16,8 @@ public class Rama2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Inicio");
+        System.out.println("Otro commit");
     }
     
 }
